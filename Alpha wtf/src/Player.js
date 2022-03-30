b493e358c5330c798f4f13c46d6a2f7228e12fe0
LCkPAn <92451567+LCkPAn@users.noreply.github.com>
2022-03-30 07:48:30 +0200

    Add files via upload

---
+++ Alpha wtf/src/Player.js
@@ -0,0 +1,73 @@
+class Player {
+
+
+    constructor(scene) {
+        this.scene=scene
+        this.cameras=scene
+        this.player = this.scene.physics.add.sprite(50, 300, 'player');
+        this.player.setBounce(0.1);
+        this.player.setCollideWorldBounds(true);
+        this.scene.physics.add.collider(this.player, this.scene.platforms);
+
+        this.cameras.main.centerOn(this.player,this.player); //la caméra se centre sur le joueur
+        this.cameras.main.useBounds=true; //la caméra ne peut pas traverser la map
+
+
+
+
+
+
+
+
+        this.scene.anims.create({
+            key: 'walk',
+            frames: this.scene.anims.generateFrameNames('player', {
+                prefix: 'robo_player_',
+                start: 2,
+                end: 3,
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.scene.anims.create({
+            key: 'idle',
+            frames: [{key: 'player', frame: 'robo_player_0'}],
+            frameRate: 10,
+
+        });
+        this.scene.anims.create({
+            key: 'jump',
+            frames: [{key: 'player', frame: 'robo_player_1'}],
+            frameRate: 10,
+            repeat:-1,
+
+        });
+    }
+
+    jump(){
+        this.player.setVelocityY(-350);
+        this.player.play('jump', true);
+    }
+    moveRight(){
+        this.player.setVelocityX(200);
+        this.player.setFlipX(false);
+        if (this.player.body.onFloor()) {
+            this.player.play('walk', true)}
+    }
+    moveLeft(){
+        this.player.setVelocityX(-200);
+        if (this.player.body.onFloor()) {
+            this.player.play('walk', true)}
+        this.player.setFlipX(true);
+    }
+    stop(){
+        this.player.setVelocityX(0);
+        if (this.player.body.onFloor()) {
+            this.player.play('idle',true)
+        }
+    }
+
+    }
+
+
+
